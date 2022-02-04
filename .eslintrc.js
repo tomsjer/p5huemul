99e7db9e8509fb25317031ff6a1420433072d7b6
tomsjer <contact@tomsjer.com>
2022-02-04 13:33:07 -0300

    Serving static assets

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,8 @@ module.exports = {
     semi: [0],
     quotes: ["error", "double"],
     "space-before-function-paren": [0],
-    "no-undef": 0
+    "no-undef": 0,
+    "no-unused-vars": 0,
+    "no-import-assign": 0
   }
 };
--- src/scripts/preload.js
+++ src/scripts/preload.js
@@ -1,3 +1,33 @@
+import {
+  // img,
+  islavectorliso,
+  grillamapa,
+  reactor,
+  auditorio,
+  cruz,
+  topografia,
+  usina,
+  reactorcenital,
+  muelle
+} from "./variables";
+
 module.exports = function preload() {
-  console.log("preload");
+  loadFont("assets/Bison-Bold.ttf");
+
+  // img = loadImage('assets/fondos/mapa-bche-extendido-ok.jpg'); // Cargar la imagen
+  // img = loadImage('assets/fondos/islafondo.png'); // Cargar la imagen
+  islavectorliso = loadImage("assets/fondos/isla_vector_liso.png"); // Cargar la imagen
+  grillamapa = loadImage("assets/masimg/grillamapa.png");
+  reactor = loadImage("assets/imgtargets/reactor-3d.png"); // Cargar la imagen
+  auditorio = loadImage("assets/imgedificios/auditorio.png"); // Cargar la imagen
+
+  cruz = loadImage("assets/masimg/cruz.png"); // Cargar la imagen
+  topografia = loadImage("assets/masimg/topografia.png"); // Cargar la imagen
+  usina = loadImage("assets/imgedificios/usina-transp.png"); // Cargar la imagen
+  reactorcenital = loadImage("assets/imgedificios/reactor-transp.png");
+  muelle = loadImage("assets/imgedificios/muelle-transp.png");
+
+  // gemelas = loadImage('assets/imgedificios/gemelas-transp.png');
+  // huemulscan = loadImage('assets/masimg/huemulscan.png');
+  // retorno = loadImage('assets/imgtargets/retorno_transp.png'); // Cargar la imagen
 };
